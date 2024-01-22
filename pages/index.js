b5b0032d1707952c61c66f720f25dab98753863c
Sowmiya04Balachandran <sowmiya2641@gmail.com>
2024-01-22 17:38:44 +0530

    deleting todo

--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,63 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-export default function Home() {
-  const [task, setTask] = useState('');
-  const [todos, setTodos] = useState([]);
-  useEffect(() => {
-    // Fetch todos from the server
-    axios.get('/api/todos').then((response) => {
-      setTodos(response.data);
-    });
-  }, []);
-  const addTodo = async () => {
-    if (task.trim() !== '') {
-      // Add todo to the server
-      const response = await axios.post('/api/todos', { task });
-      setTodos([...todos, response.data]);
-      setTask('');
-    }
-  };
-  const toggleTodo = async (id) => {
-    const response = await axios.put(`/api/todos/${id}`);
-    setTodos((prevTodos) =>
-      prevTodos.map((todo) =>
-        todo._id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
-  };  
 
-  const deleteTodo = async (id) => {
-    // Delete todo from the server
-    await axios.delete(`/api/todos/${id}`);
-    setTodos((prevTodos) => prevTodos.filter((todo) => todo._id !== id));
-  };
-  return (
-    <div>
-      <h1>Todo List</h1>
-      <input
-        type="text"
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
-      />
-      <button onClick={addTodo}>Add Todo</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo._id}>
+export default function Home() {
+    const [task, setTask] = useState('');
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        // Fetch todos from the server
+        axios.get('/api/todos').then((response) => {
+            setTodos(response.data);
+        });
+    }, []);
+
+    const addTodo = async () => {
+        if (task.trim() !== '') {
+            // Add todo to the server
+            const response = await axios.post('/api/todos', { task });
+            setTodos([...todos, response.data]);
+            setTask('');
+        }
+    };
+
+    const toggleTodo = async (id) => {
+        const response = await axios.put(`/api/todos/${id}`);
+        setTodos((prevTodos) =>
+            prevTodos.map((todo) =>
+                todo._id === id ? { ...todo, completed: !todo.completed } : todo
+            )
+        );
+    };  
+
+    const deleteTodo = async (id) => {
+        // Delete todo from the server
+        await axios.delete(`/api/todos/${id}`);
+        setTodos((prevTodos) => prevTodos.filter((todo) => todo._id !== id));
+    };
+
+    return (
+        <div>
+            <h1>Todo List</h1>
             <input
-              type="checkbox"
-              checked={todo.completed}
-              onChange={() => toggleTodo(todo._id)}
+                type="text"
+                value={task}
+                onChange={(e) => setTask(e.target.value)}
             />
-            <span>{todo.task}</span>
-            <button onClick={() => deleteTodo(todo._id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+            <button onClick={addTodo}>Add Todo</button>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo._id}>
+                        <input
+                            type="checkbox"
+                            checked={todo.completed}
+                            onChange={() => toggleTodo(todo._id)}
+                        />
+                        <span>{todo.task}</span>
+                        <button onClick={() => deleteTodo(todo._id)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
--- server.js
+++ server.js
@@ -4,37 +4,44 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(bodyParser.json());
+
 mongoose.connect('mongodb+srv://sowmiya:fFI6LTEYs2VQEluk@todolist.pnhjdjq.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
+
 const todoSchema = new mongoose.Schema({
-  task: String,
-  completed: Boolean,
+    task: String,
+    completed: Boolean,
 });
+
 const Todo = mongoose.model('Todo', todoSchema);
+
 app.get('/api/todos', async (req, res) => {
-  const todos = await Todo.find();
-  res.json(todos);
+    const todos = await Todo.find();
+    res.json(todos);
 });
+
 app.post('/api/todos', async (req, res) => {
-  const { task } = req.body;
-  const todo = new Todo({ task, completed: false });
-  await todo.save();
-  res.json(todo);
+    const { task } = req.body;
+    const todo = new Todo({ task, completed: false });
+    await todo.save();
+    res.json(todo);
 });
 
 app.put('/api/todos/:id', async (req, res) => {
     const { id } = req.params;
     const todo = await Todo.findByIdAndUpdate(id, { $set: { completed: !req.body.completed } }, { new: true });
     res.json(todo);
-  });
-
-app.delete('/api/todos/:id', async (req, res) => {
-  const { id } = req.params;
-  await Todo.findByIdAndDelete(id);
-  res.json({ success: true });
 });
+
+const deleteTodo = async (id) => {
+    // Delete todo from the server
+    await axios.delete(`/api/todos/${id}`);
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo._id !== id));
+};
+
+
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
