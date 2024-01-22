b1b2730f9dbe1c3ccd7d9af5b17d213274d25e7d
Sowmiya04Balachandran <sowmiya2641@gmail.com>
2024-01-22 17:25:21 +0530

    time to save

--- server.js
+++ server.js
@@ -1,36 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost/todo-list', {
+mongoose.connect('mongodb+srv://ponmathittl:fFI6LTEYs2VQEluk@todolist.pnhjdjq.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 
 const todoSchema = new mongoose.Schema({
   task: String,
   completed: Boolean,
 });
-
 const Todo = mongoose.model('Todo', todoSchema);
-
 app.get('/api/todos', async (req, res) => {
   const todos = await Todo.find();
   res.json(todos);
 });
-
 app.post('/api/todos', async (req, res) => {
   const { task } = req.body;
   const todo = new Todo({ task, completed: false });
   await todo.save();
   res.json(todo);
 });
-
 app.put('/api/todos/:id', async (req, res) => {
   const { id } = req.params;
   const todo = await Todo.findById(id);
@@ -38,13 +34,11 @@ app.put('/api/todos/:id', async (req, res) => {
   await todo.save();
   res.json(todo);
 });
-
 app.delete('/api/todos/:id', async (req, res) => {
   const { id } = req.params;
   await Todo.findByIdAndDelete(id);
   res.json({ success: true });
 });
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
